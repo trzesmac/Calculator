e24e46212b599e0405e637f535a8ade6e46e305f
igort <igort@vp.pl>
2025-12-30 22:20:14 +0100

    Add additional function

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ public class Program
 	static void Main(string[] args)
 	{
 		Console.WriteLine("Prosty kalkulator");
+		Console.WriteLine($"10 + 5 = {Add(10, 5)}");
+	}
+	
+	static int Add(int x, int y)
+	{
+		return x + y;
 	}
 }
