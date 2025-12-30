39881a307f725bf51161a50a8b8538419ab26ee4
igort <igort@vp.pl>
2025-12-30 22:48:57 +0100

    Added multiply function

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ public class Program
 		Console.WriteLine("Prosty kalkulator");
 		Console.WriteLine($"10 + 5 = {Add(10, 5)}");
 		Console.WriteLine($"10 - 5 = {Substract(25, 5)}");
+		Console.WriteLine($"5 * 5 = {Multiply(25, 5)}");
 	}
 	
 	static int Add(int x, int y)
@@ -15,6 +16,11 @@ public class Program
 	}
 	
 	static int Substract(int x, int y)
+	{
+		return x - y;
+	}
+	
+	static int Multiply(int x, int y)
 	{
 		return x - y;
 	}
