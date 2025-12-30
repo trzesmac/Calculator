cf98db71c17f86a7c0afdac670453cb3a762a2e4
igort <igort@vp.pl>
2025-12-30 22:24:57 +0100

    Added Sybstract function

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ public class Program
 	{
 		Console.WriteLine("Prosty kalkulator");
 		Console.WriteLine($"10 + 5 = {Add(10, 5)}");
+		Console.WriteLine($"10 - 5 = {Substract(25, 5)}");
 	}
 	
 	static int Add(int x, int y)
 	{
 		return x + y;
+	}
+	
+	static int Substract(int x, int y)
+	{
+		return x - y;
 	}
 }
